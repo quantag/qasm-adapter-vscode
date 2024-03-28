3b5a6bd77412e36aedef0ec944aa5d2d03e03b6a
Sergii Salata <sergii.salata@quantag-it.com>
2024-03-28 11:08:34 +0100

    updated sample workplace

--- sampleWorkspace/tket/tket1.py
+++
@@ -1,8 +0,0 @@
-from pytket import Circuit
-
-c = Circuit(2, 2) # define a circuit with 2 qubits and 2 bits
-c.H(0)            # add a Hadamard gate to qubit 0
-c.Rz(0.25, 0)     # add an Rz gate of angle 0.25*pi to qubit 0
-c.CX(1,0)         # add a CX gate with control qubit 1 and target qubit 0
-c.measure_all()   # measure qubits 0 and 1, recording the results in bits 0 and 1
-
